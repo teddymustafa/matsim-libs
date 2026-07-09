f8c09b87f0b0a152ab4705b68b8247369a0a044a
teddymustafa <teddymustafa26@gmail.com>
2026-07-09 16:39:04 +0200

    ElasticityTest.java erstellt und neue Funktionalitäten in ElasticityAnalysis.java hinzugefügt (manche Werten aus config.xml auslesen lassen).

--- contribs/application/src/main/java/org/matsim/application/analysis/population/ElasticityAnalysis.java
+++ contribs/application/src/main/java/org/matsim/application/analysis/population/ElasticityAnalysis.java
@@ -1,5 +1,8 @@
 package org.matsim.application.analysis.population;
 
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -7,21 +10,40 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 class ElasticityAnalysis {
 	public static void main() {
 
 		//TODO: dont use absolute path for interoperability
-		File inputFile = new File(); // ABSOLUTER PFAD HIER
-		File outputFile = new File(); // ABSOLUTER PFAD HIER
+
+		File configFile = new File("/home/teddymustafa/Desktop/FG-VSP/elasticity/berlin-v7.1-1pct.output_config.xml");
+		Config config = ConfigUtils.loadConfig(configFile.getPath());
+
+		File inputFile = new File("/home/teddymustafa/Desktop/FG-VSP/elasticity/berlin-v7.1-1pct.output_trips.csv.gz"); // ABSOLUTER PFAD HIER
+		File outputFile = new File("home/teddymustafa/Desktop/FG-VSP/elasticity/analysis/elasticity/elasticity.csv"); // ABSOLUTER PFAD HIER
+
 
 		String delimiter = ";";
-		//TODO: selber in config.xml suchen
-		double pricePerMeter = 1.49e-4;
-		double mutts = 1.0;
 		//TODO: Commandline Argument Funktionalität hinzufügen
 		Set<String> relevantModes = Set.of("car", "ride");
+		Map<String, Double> pricePerMeterByMode = new HashMap<>();
+
+		for (String mode : relevantModes) {
+			double rate = config.scoring()
+					.getScoringParameters(null)
+					.getModes()
+					.get(mode)
+					.getMonetaryDistanceRate();
+
+			System.out.println("mode = " + mode + ", monetaryDistanceRate = " + rate);
+
+			pricePerMeterByMode.put(mode, rate);
+		}
+
+
+		//TODO: selber in config.xml suchen
+		double marginalUtilityOfMoney = config.scoring().getScoringParameters(null).getMarginalUtilityOfMoney();
+
 
 		// creates any missing folders in the path
 		outputFile.getParentFile().mkdirs();
@@ -37,14 +59,19 @@ class ElasticityAnalysis {
 			) {
 
 
+			// Checks for header
 			String headerLine = in.readLine();
+
+			// Array that stores header
 			String[] header = headerLine.split(delimiter);
 
+			// a placeholder its impossible for array to possess negative value*
 			int idxPerson = -1;
 			int idxTravDist = -1;
 			int idxMainMode = -1;
 			int idxLongestMode = -1;
 
+			//checks for position of the relevant header and consequently the column
 			for(int i = 0; i < header.length; i++){
 				if (header[i].equals("person")) idxPerson = i;
 				if (header[i].equals("traveled_distance")) idxTravDist = i;
@@ -52,20 +79,22 @@ class ElasticityAnalysis {
 				if (header[i].equals("longest_distance_mode")) idxLongestMode = i;
 			}
 
+			// *which is relevant for this IllegalStateException
 			if(idxPerson == -1  || idxMainMode == -1 || idxTravDist == -1 || idxLongestMode == -1 ){
 				throw new IllegalStateException(
 					"Expected column not found in header:" + headerLine);
 			}
 
-			// Speichert Summe von traveled_distance je nach modes
+			// Map that stores keystring "mode" and double "sum of distance"
 			Map<String, Double> sumDistMode = new HashMap<>();
-			// Speichert Summe von nPersons je nach modes
+			// Map that stores keystring "mode" and set of  "personsByMode"
 			Map<String, Set<String>> personsByMode = new HashMap<>();
 			for (String mode : relevantModes){
 				sumDistMode.put(mode, 0.0);
 				personsByMode.put(mode, new HashSet<>());
 			}
 
+			// lets no duplicate
 			Set<String> allPersons = new HashSet<>();
 
 			String line;
@@ -77,12 +106,13 @@ class ElasticityAnalysis {
 				String[] row = line.split(delimiter);
 
 				String person = row[idxPerson];
+
 				allPersons.add(person);
 
 				String mainMode = row[idxMainMode];
 				String longestMode = row[idxLongestMode];
 
-				// only keep rows where BOTH columns are car/ride
+				// filter(mainmode == car,ride && longestmode == car,ride )
 				if(relevantModes.contains(mainMode) && relevantModes.contains(longestMode)){
 					double distance = Double.parseDouble(row[idxTravDist]);
 					sumDistMode.put(mainMode, sumDistMode.get(mainMode) + distance);
@@ -100,9 +130,12 @@ class ElasticityAnalysis {
 				double sumDistance = sumDistMode.get(mode);
 
 				double avgDistance = sumDistance/nPersonsMode;
+
+				double pricePerMeter = pricePerMeterByMode.get(mode);
 				double price = avgDistance*pricePerMeter;
+				// anzahl trips bestimmter mode durch gesamtzahl des trips
 				double modeShare = (double) nPersonsMode / nPersonsTotal;
-				double elasticity = -mutts * (price * (1 - modeShare));
+				double elasticity = -marginalUtilityOfMoney * (price * (1 - modeShare));
 
 				out.write(mode + "," + elasticity);
 				out.newLine();
---
+++ contribs/application/src/test/java/org/matsim/application/analysis/population/ElasticityTest.java
@@ -0,0 +1,4 @@
+package org.matsim.application.analysis.population;
+
+public class ElasticityTest {
+}
