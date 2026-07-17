80fd189922b91deb9184479ea82ceac6337f6238
teddymustafa <teddymustafa26@gmail.com>
2026-07-17 17:25:32 +0200

    first functional version when ran with command line arguments

--- contribs/application/src/main/java/org/matsim/application/analysis/population/ElasticityAnalysis.java
+++ contribs/application/src/main/java/org/matsim/application/analysis/population/ElasticityAnalysis.java
@@ -41,7 +41,7 @@ import tech.tablesaw.io.csv.CsvReadOptions;
 	description = "Generates statistics for elasticity."
 )
 @CommandSpec(
-	requires = {"trips.csv", "mode_share.csv"},
+	requires = {"trips.csv"},
 	produces = {"elasticity_stats.csv"}
 	// trips_curated.csv is just a Zwischenschritt tbh
 )
@@ -57,69 +57,90 @@ public class ElasticityAnalysis implements MATSimAppCommand {
 	private final InputOptions input = InputOptions.ofCommand(ElasticityAnalysis.class);
 	@CommandLine.Mixin
 	private final OutputOptions output = OutputOptions.ofCommand(ElasticityAnalysis.class);
-
-	@CommandLine.Option(
-		names = "--modes-filter",
-		split = ",",
-		description = "Define which modes should be included into elasticity analysis."
-	)
+	@CommandLine.Option(names = "--modes-filter", split = ",", description = "Define which modes should be included into elasticity analysis.")
 	private Set<String> modes;
 
-	@CommandLine.Option(
-		names = "--group-by",
-		split = ",",
-		description = "Define which Group of Population should be included into elasticity analysis."
-	)
-	private String groupBy;
+//	// HARDCODING FOR TESTING PURPOSE
+//	private static final String TRIPS_PATH = "/home/teddymustafa/Desktop/FG-VSP/elasticity/berlin-v7.1-1pct.output_trips.csv.gz";
+//	private static final String STATS_OUT_PATH = "/home/teddymustafa/Desktop/FG-VSP/elasticity/analysis/elasticity/elasticity_stats.csv";
+//	private final Set<String> modes = Set.of("car","ride");
+//	private final String groupBy = null;
 
+	// Shared state across stages -> fields
 	private static final double BETA_MONEY = config.scoring().getScoringParameters(null).getMarginalUtilityOfMoney();
-	private int tripCount;
-
-
-	// TRIPS PER MODE
-	private final Object2IntMap<String> tripsPerMode = new Object2IntOpenHashMap<>();
-	// DISTANCE PER MODE
-	private final Object2DoubleMap<String> distancePerMode = new Object2DoubleOpenHashMap<>();
-	// Group trips
-	private final Map<String, Object2IntOpenHashMap<String>> tripsPerGroup = new HashMap<>();
-	// Group traveled_distance
-	private final Map<String, Object2IntOpenHashMap<String>> distancePerGroup = new HashMap<>();
-	private final Map<String, Set<String>> personsByMode = new HashMap<>();
+	private Table tripsCurated;
+	private Table tripsMode;
 
 	// ERGEBNIS
 	private final Object2DoubleMap<String> elasticity = new Object2DoubleOpenHashMap<>();
+	private final Object2IntMap<String> nPersons = new Object2IntOpenHashMap<>();
 
-	private Table tripsMode;
 
-	public static void main() {
-		new ElasticityAnalysis().execute();
+	public static void main(String[] args) throws Exception {
+//		new ElasticityAnalysis().call();
+		new ElasticityAnalysis().execute(args);
 	}
 
 	@Override
 	public Integer call() throws Exception {
 
+		tripsCurated = Table.read().csv(
+			CsvReadOptions.builder(IOUtils.getBufferedReader(input.getPath("trips.csv")))
+				.columnTypesPartial(getColumnTypes())
+				.sample(false)
+				.separator(CsvOptions.detectDelimiter(input.getPath("trips.csv")))
+				.build());
 
+		if (modes == null || modes.isEmpty()){
+			tripsMode = tripsCurated;
+		} else {
+		tripsMode = tripsCurated.where(
+			tripsCurated.stringColumn("main_mode").isIn(modes));
+		}
 
-		Table tripsCurated = Table.read().csv(CsvReadOptions.builder(IOUtils.getBufferedReader(input.getPath("trips.csv"))).columnTypesPartial(getColumnTypes()).sample(false).separator(CsvOptions.detectDelimiter(input.getPath("trips.csv"))).build());
-		double sumDist = tripsCurated.longColumn("traveled_distance").sum();
-		int nPersons = tripsCurated.stringColumn("person").countUnique();
-
-		this.tripsMode = tripsCurated.where(
-			tripsCurated.stringColumn("main_mode").isIn(modes)
-		);
-
-		if(groupBy  != null && groupBy.isBlank())
-			throw new IllegalArgumentException("argument was given without a usable value.");
+		calculatenInfo();
+		log.info("nPersonsTotal successfully calculated");
+		calculatenPersons(modes);
+		log.info("nPersons successfully calculated");
+		calculateElasticityByMode(modes);
+		log.info("calculated elasticity");
+		writeElasticityStats();
 
 		return 0;
 	}
 
 	private static Map<String, ColumnType> getColumnTypes() {
-		Map<String, ColumnType> columnTypes = new HashMap<>(Map.of("person", ColumnType.STRING,"main_mode", ColumnType.STRING));
+		Map<String, ColumnType> columnTypes = new HashMap<>(Map.of(
+			"person", ColumnType.STRING,
+			"main_mode", ColumnType.STRING,
+			"trip_number", ColumnType.INTEGER));
 
-		columnTypes.put("traveled_distance", ColumnType.LONG);
+		columnTypes.put("traveled_distance", ColumnType.DOUBLE);
 
 		return columnTypes;
+	}
+
+	private void calculatenInfo(){
+			int nPerson = tripsCurated.stringColumn("person").countUnique();
+			int trips = tripsCurated.rowCount();
+			int tripsCar = 	tripsCurated.where(tripsCurated.stringColumn("main_mode").isEqualTo("car")).rowCount();
+			int tripsRide = tripsCurated.where(tripsCurated.stringColumn("main_mode").isEqualTo("ride")).rowCount();
+			log.info("nPersonsTotal is "+ nPerson);
+			log.info("nTripsTotal is "+ trips);
+			log.info("nTripsCar is "+ tripsCar);
+			log.info("nTripsRide is "+ tripsRide);
+	}
+
+	private void calculatenPersons(Set<String> modes){
+		for (String mode: modes){
+
+			int nPerson = tripsMode.where(tripsMode.stringColumn("main_mode").isEqualTo(mode))
+				.stringColumn("person")
+				.countUnique();
+			nPersons.put(mode, nPerson);
+			log.info("nPersons for "+ mode +" = "+ nPerson);
+
+		}
 	}
 
 
@@ -140,43 +161,50 @@ public class ElasticityAnalysis implements MATSimAppCommand {
 	 * calculate ModeShare
 	 * */
 	private double calculateModeSharePerMode(String mode) throws IOException{
-		// nPersonsMode
+		double tripsOfMode = tripsCurated.stringColumn("main_mode").countOccurrences(mode);
+		double modeShare = tripsOfMode / tripsCurated.rowCount();
+		log.info("modeShare for "+ mode +" = "+ modeShare);
+		return modeShare;
 	}
 	/**
 	 * calculate Monetary cost per trip by mode
 	 * */
 	private double calculateMonetaryCostPerTripPerMode(String mode) throws IOException{
-		double avgDist = tripsMode.longColumn("traveled_distance").where(tripsMode.stringColumn("main_mode").isEqualTo(mode)).mean();
-		return calculateMonetaryDistanceRateByMode(mode) * avgDist;
+		double avgDist = tripsMode.doubleColumn("traveled_distance")
+			.where(tripsMode.stringColumn("main_mode").isEqualTo(mode))
+			.mean();
+		double price = calculateMonetaryDistanceRateByMode(mode) * avgDist;
+		log.info("monetaryCostPerTrip for "+ mode +" = "+ price);
+		return price;
 	}
 
 	/**
 	* calculate elasticity by mode.
 	 */
-	private void calculateElasticityByMode(Set<String> relevantModes) throws IOException {
+	private void calculateElasticityByMode(Set<String> modes) throws IOException {
 
 		 for (String mode: modes){
 
-			 double modeShare = calculateModeShare(mode); // personsByMode.get(mode).size();
-			 double pricePerMeter = calculatePricePerMeterByMode(mode);
-			 double price = calculatePrice(mode);
-			 double elasticity = -BETA_MONEY * (price * (1-modeShare));
+			 double modeShare = calculateModeSharePerMode(mode); // personsByMode.get(mode).size();
+			 double price = calculateMonetaryCostPerTripPerMode(mode);
+			 double e = -BETA_MONEY * (price * (1-modeShare));
+			 log.info("elasticity for {} = {}", mode, e);
+			 elasticity.put(mode, e);
 
 		 }
 	}
 
-	private void writeElasticityStats() throws IOException{}
-	private void analyseAndWriteElasticityStatsPerGroup() throws IOException{}
-
-	private Map<String, List<String>> getGroupsOfSubpopulations(Map<String, String> groupsOfSubpopulationsRaw) {
-		Map<String, List<String>> groupsOfSubpopulations = new HashMap<>();
-		for (Map.Entry<String, String> entry : groupsOfSubpopulationsRaw.entrySet()) {
-			List<String> subpops = Arrays.asList(entry.getValue().split(","));
-			groupsOfSubpopulations.put(entry.getKey(), subpops);
+	private void writeElasticityStats() throws IOException{
+		try (BufferedWriter writer = IOUtils.getBufferedWriter(output.getPath("elasticity_stats.csv").toString())){
+			writer.write("mode;nPersons;elasticity");
+			writer.newLine();
+			for (String mode:modes){
+				int trips = tripsMode.stringColumn("main_mode").countOccurrences(mode);
+				writer.write(mode + ";" + nPersons.getInt(mode) + ";" + elasticity.getDouble(mode));
+				writer.newLine();
+			}
 		}
-		return groupsOfSubpopulations;
+		log.info("write complete!");
 	}
-
-
 
 }
