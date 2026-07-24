8ec1c0d481c37950357cabb94e1e7c7ea642dad9
teddymustafa <teddymustafa26@gmail.com>
2026-07-24 11:16:11 +0200

    changes from Jour Fixe (constants for columntype)

--- contribs/application/src/main/java/org/matsim/application/analysis/population/ElasticityAnalysis.java
+++ contribs/application/src/main/java/org/matsim/application/analysis/population/ElasticityAnalysis.java
@@ -34,6 +34,10 @@ public class ElasticityAnalysis implements MATSimAppCommand {
 
 	// Creating Log
 	private static final Logger log = LogManager.getLogger(ElasticityAnalysis.class);
+	private static final String MAIN_MODE = "main_mode";
+	public static final String PERSON = "person";
+	public static final String TRIP_NUMBER = "trip_number";
+	public static final String TRAVELED_DISTANCE = "traveled_distance";
 
 	@CommandLine.Mixin
 	private final InputOptions input = InputOptions.ofCommand(ElasticityAnalysis.class);
@@ -98,11 +102,10 @@ public class ElasticityAnalysis implements MATSimAppCommand {
 
 	private static Map<String, ColumnType> getColumnTypes() {
 		Map<String, ColumnType> columnTypes = new HashMap<>(Map.of(
-			"person", ColumnType.STRING,
-			"main_mode", ColumnType.STRING,
-			"trip_number", ColumnType.INTEGER));
-
-		columnTypes.put("traveled_distance", ColumnType.DOUBLE);
+			PERSON, ColumnType.STRING,
+			MAIN_MODE, ColumnType.STRING,
+			TRIP_NUMBER, ColumnType.INTEGER,
+			TRAVELED_DISTANCE, ColumnType.DOUBLE));
 
 		return columnTypes;
 	}
@@ -112,19 +115,19 @@ public class ElasticityAnalysis implements MATSimAppCommand {
 		log.info("nTripsTotal is "+ nTripsTotal);
 		for (String mode: modes){
 
-			int trip = tripsCurated.where(tripsCurated.stringColumn("main_mode").isEqualTo(mode)).rowCount();
+			int trip = tripsCurated.where(tripsCurated.stringColumn(MAIN_MODE).isEqualTo(mode)).rowCount();
 			nTrips.put(mode, trip);
 			log.info("nTrips for "+ mode +" = "+ trip);
 		}
 	}
 
 	private void calcnPersons(Set<String> modes){
-		int nPersonTotal = tripsCurated.stringColumn("person").countUnique();
+		int nPersonTotal = tripsCurated.stringColumn(PERSON).countUnique();
 		log.info("nPersonTotal is "+ nPersonTotal);
 		for (String mode: modes){
 
-			int person = tripsCurated.where(tripsCurated.stringColumn("main_mode").isEqualTo(mode))
-				.stringColumn("person")
+			int person = tripsCurated.where(tripsCurated.stringColumn(MAIN_MODE).isEqualTo(mode))
+				.stringColumn(PERSON)
 				.countUnique();
 			nPersons.put(mode, person);
 			log.info("nPersons for "+ mode +" = "+ person);
@@ -151,7 +154,7 @@ public class ElasticityAnalysis implements MATSimAppCommand {
 
 		for(String mode: modes ){
 			int tripsOfMode = nTrips.getInt(mode);
-			double share = (double) tripsOfMode / tripsCurated.rowCount();
+			double share = (double) tripsOfMode / tripsCurated.rowCount(); // nur Persons in tripsCurated filtrieren
 			modeShare.put(mode, share);
 			log.info("modeShare for "+ mode +" = "+ share);
 		}
@@ -163,8 +166,8 @@ public class ElasticityAnalysis implements MATSimAppCommand {
 	private void calcAvgDistPerMode(Set<String> modes) throws IOException{
 
 		for(String mode : modes){
-			double avgDist = tripsCurated.doubleColumn("traveled_distance")
-				.where(tripsCurated.stringColumn("main_mode").isEqualTo(mode))
+			double avgDist = tripsCurated.doubleColumn(TRAVELED_DISTANCE)
+				.where(tripsCurated.stringColumn(MAIN_MODE).isEqualTo(mode))
 				.mean();
 			avgDistance.put(mode, avgDist);
 			log.info("avgDistance for "+ mode +" = "+ avgDist);
@@ -198,17 +201,21 @@ public class ElasticityAnalysis implements MATSimAppCommand {
 	}
 
 	/**
-	 * calculate elasticity by mode.
+	 * write elasticity_stats.csv
 	 */
 	private void writeElasticityStats() throws IOException{
 		try (BufferedWriter writer = IOUtils.getBufferedWriter(output.getPath("elasticity_stats.csv").toString())){
+
+			writer.write("#Total persons: " + tripsCurated.stringColumn(PERSON).countUnique());
+			writer.newLine();
+			writer.write("#Total trips: " + tripsCurated.rowCount());
+			writer.newLine();
 			writer.write("mode;nPersons;nTrips;modeshare;monetarycost;avg_distance;elasticity");
 			writer.newLine();
 			for (String mode:modes){
 				writer.write(mode + ";" + nPersons.getInt(mode) + ";" + nTrips.getInt(mode) + ";" + modeShare.getDouble(mode) + ";" + monetaryCost.getDouble(mode) + ";" + avgDistance.getDouble(mode)+ ";" +elasticity.getDouble(mode));
 				writer.newLine();
 			}
-//			writer.write("Total" + ";" + tripsCurated.stringColumn("person").countUnique() + ";" + tripsCurated.rowCount() + ";" + " " + ";" + " " + ";" + " " + ";" + " " + ";");
 		}
 		log.info("write complete!");
 	}
